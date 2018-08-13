d1c266d79feb0115126e62f4920886fb37c92752
Nina Geiger <nina.geiger@yatta.de>
2018-08-13 11:32:20 +0200

    introduce simple smoke test

--- src/test/java/de/yatta/todoapp/TodoappApplicationTests.java
+++ src/test/java/de/yatta/todoapp/TodoappApplicationTests.java
@@ -1,16 +1,30 @@
 package de.yatta.todoapp;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import de.yatta.todoapp.controller.TodoController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TodoappApplicationTests {
-
+	
+	@Autowired
+	private TodoController controller;
+	
 	@Test
 	public void contextLoads() {
+		assertNotNull(controller);;
 	}
+	
+	
+	
 
 }

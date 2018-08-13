9838ee2ca4ef3b170e15c42481fa39b7b8aab5c5
Nina Geiger <nina.geiger@yatta.de>
2018-08-13 13:50:29 +0200

    remove some annotations

--- src/test/java/de/yatta/todoapp/MockedMongoConfig.java
+++ src/test/java/de/yatta/todoapp/MockedMongoConfig.java
@@ -3,25 +3,21 @@ package de.yatta.todoapp;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import com.github.fakemongo.Fongo;
 import com.mongodb.MongoClient;
 
 
-@Configuration
-@EnableMongoRepositories
 @ComponentScan
 @Profile("test")
 public class MockedMongoConfig extends AbstractMongoConfiguration{
 
 	@Override
+	@Bean
 	public MongoClient mongoClient() {
-		Fongo fongo = new Fongo(getDatabaseName());
+		Fongo fongo = new Fongo("Fongo");
 		return fongo.getMongo();
 	}
 
@@ -30,9 +26,5 @@ public class MockedMongoConfig extends AbstractMongoConfiguration{
 		return "todoapp";
 	}
 	
-	@Bean
-	public MongoTemplate mongoTemplate() {
-		return new MongoTemplate(mongoClient(), getDatabaseName());
-	}
 
 }

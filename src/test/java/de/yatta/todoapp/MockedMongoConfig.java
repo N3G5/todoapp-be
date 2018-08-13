57fa4dafa18b4aae73e6c5c83ee9dca5f9d81d3d
Nina Geiger <nina.geiger@yatta.de>
2018-08-13 13:37:32 +0200

    testing

--- src/test/java/de/yatta/todoapp/MockedMongoConfig.java
+++ src/test/java/de/yatta/todoapp/MockedMongoConfig.java
@@ -2,15 +2,19 @@ package de.yatta.todoapp;
 
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import com.github.fakemongo.Fongo;
 import com.mongodb.MongoClient;
 
 
 @Configuration
+@EnableMongoRepositories
+@ComponentScan
 public class MockedMongoConfig extends AbstractMongoConfiguration{
 
 	@Override

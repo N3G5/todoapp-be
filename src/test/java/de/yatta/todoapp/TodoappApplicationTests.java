a2cfe5f2d55d7f52c29fb9cdc24605636f97e93b
Nina Geiger <nina.geiger@yatta.de>
2018-08-13 16:33:00 +0200

    add configuration for context

--- src/test/java/de/yatta/todoapp/TodoappApplicationTests.java
+++ src/test/java/de/yatta/todoapp/TodoappApplicationTests.java
@@ -24,6 +24,7 @@ import de.yatta.todoapp.repositories.TodoRepository;
 @DataMongoTest
 @TestPropertySource("classpath:application.test.properties")
 @ActiveProfiles("test")
+@ContextConfiguration(classes = TodoappApplication.class)
 public class TodoappApplicationTests {
 	
 	@Autowired TodoRepository todoRepo;

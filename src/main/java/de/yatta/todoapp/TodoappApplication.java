75b99227ccf056446f88434c5aad3733b7969648
Nina Geiger <nina.geiger@yatta.de>
2018-08-13 16:37:51 +0200

    add component scan annotation

--- src/main/java/de/yatta/todoapp/TodoappApplication.java
+++ src/main/java/de/yatta/todoapp/TodoappApplication.java
@@ -2,8 +2,10 @@ package de.yatta.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "de.yatta.todoapp")
 public class TodoappApplication {
 
 	public static void main(String[] args) {

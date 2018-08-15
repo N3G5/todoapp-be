c8b5c09f9bf3a0cc8e3538ffb7ca44ab2f2032ef
Nina Geiger <nina.geiger@yatta.de>
2018-08-15 12:03:16 +0200

    rename test Method

--- src/test/java/de/yatta/todoapp/TodoappApplicationTests.java
+++ src/test/java/de/yatta/todoapp/TodoappApplicationTests.java
@@ -2,18 +2,14 @@ package de.yatta.todoapp;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import de.yatta.todoapp.controller.TodoController;
@@ -35,10 +31,11 @@ public class TodoappApplicationTests {
 	@Test
 	public void contextLoads() {
 		assertNotNull(controller);
+		assertNotNull(todoRepo);
 	}
 	
 	@Test
-	public void testMethod() {
+	public void testSaveInRepo() {
 		Todo todo = todoRepo.save(new Todo("First todo"));
 		Todo foundTodo = todoRepo.findById(todo.getId()).get();
 		assertNotNull(foundTodo);
